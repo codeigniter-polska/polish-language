acf4682d8740a59aa20d43bad55d3b703fe26fbc
michalsn <michal@sniatala.pl>
2012-08-31 22:09:40 +0200

    poprawki formatowania

--- number_lang.php
+++ number_lang.php
@@ -1,10 +1,10 @@
 <?php
 
-$lang['terabyte_abbr'] 	= "TB";
-$lang['gigabyte_abbr'] 	= "GB";
-$lang['megabyte_abbr'] 	= "MB";
-$lang['kilobyte_abbr'] 	= "KB";
-$lang['bytes'] 			= "Bajtów";
+$lang['terabyte_abbr'] = "TB";
+$lang['gigabyte_abbr'] = "GB";
+$lang['megabyte_abbr'] = "MB";
+$lang['kilobyte_abbr'] = "KB";
+$lang['bytes']         = "Bajtów";
 
 /* End of file number_lang.php */
 /* Location: ./system/language/polish/number_lang.php */
--- unit_test_lang.php
+++ unit_test_lang.php
@@ -1,24 +1,24 @@
 <?php
 
-$lang['ut_test_name']		= 'Nazwa testu';
-$lang['ut_test_datatype']	= 'Testowy typ';
-$lang['ut_res_datatype']	= 'Oczekiwany typ';
-$lang['ut_result']			= 'Wynik';
-$lang['ut_undefined']		= 'Niezidentyfikowana nazwa testu';
-$lang['ut_file']			= 'Nazwa pliku';
-$lang['ut_line']			= 'Numer linii';
-$lang['ut_passed']			= 'Zaliczony';
-$lang['ut_failed']			= 'Niezaliczony';
-$lang['ut_boolean']			= 'Boolean';
-$lang['ut_integer']			= 'Integer';
-$lang['ut_float']			= 'Float';
-$lang['ut_double']			= 'Float'; // może być taki sam jak float
-$lang['ut_string']			= 'String';
-$lang['ut_array']			= 'Array';
-$lang['ut_object']			= 'Object';
-$lang['ut_resource']		= 'Resource';
-$lang['ut_null']			= 'Null';
-$lang['ut_notes']			= 'Notes';
+$lang['ut_test_name']     = 'Nazwa testu';
+$lang['ut_test_datatype'] = 'Testowy typ';
+$lang['ut_res_datatype']  = 'Oczekiwany typ';
+$lang['ut_result']        = 'Wynik';
+$lang['ut_undefined']     = 'Niezidentyfikowana nazwa testu';
+$lang['ut_file']          = 'Nazwa pliku';
+$lang['ut_line']          = 'Numer linii';
+$lang['ut_passed']        = 'Zaliczony';
+$lang['ut_failed']        = 'Niezaliczony';
+$lang['ut_boolean']       = 'Boolean';
+$lang['ut_integer']       = 'Integer';
+$lang['ut_float']         = 'Float';
+$lang['ut_double']        = 'Float'; // może być taki sam jak float
+$lang['ut_string']        = 'String';
+$lang['ut_array']         = 'Array';
+$lang['ut_object']        = 'Object';
+$lang['ut_resource']      = 'Resource';
+$lang['ut_null']          = 'Null';
+$lang['ut_notes']         = 'Notes';
 
 
 /* End of file unit_test_lang.php */
