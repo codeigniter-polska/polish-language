75bd1829c51312c8529e950d97a837ec18c5b2ef
Slawomir Jasinski <slav123@gmail.com>
2012-09-06 09:42:04 +1000

    z polskiego na polski

--- ftp_lang.php
+++ ftp_lang.php
@@ -1,8 +1,8 @@
 <?php
 
 $lang['ftp_no_connection']       = "Nie można zlokalizować poprawnego ID połączenia. Upewnij się że połączenie zostało ustanowione zanim zaczniesz wykonywać jakiekolwiek operacje z plikami.";
-$lang['ftp_unable_to_connect']   = "Nie można połączyć się to Twojego serwera FTP używając podanej nazwy hosta.";
-$lang['ftp_unable_to_login']     = "Nie można zalogować się do Twojego serwera FTP. Proszę sprawdzić nazwę użytkownika oraz hasło.";
+$lang['ftp_unable_to_connect']   = "Nie można połączyć się z serwerem FTP używając podanej nazwy hosta.";
+$lang['ftp_unable_to_login']     = "Nie można zalogować się do serwera FTP. Proszę sprawdzić nazwę użytkownika oraz hasło.";
 $lang['ftp_unable_to_makdir']    = "Nie można utworzyć folderu o podanej nazwie.";
 $lang['ftp_unable_to_changedir'] = "Nie można zmienić folderu.";
 $lang['ftp_unable_to_chmod']     = "Nie można ustawić praw dostępu dla pliku. Proszę sprawdzić ścieżkę dostępu. Uwaga: Ta funkcjonalność jest dostępna od PHP 5.";
