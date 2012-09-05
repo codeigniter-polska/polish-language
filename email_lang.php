602154d805ca2e40fa02bf9f58c0005a55ae1568
Slawomir Jasinski <slav123@gmail.com>
2012-09-06 09:39:40 +1000

    poprawki literowek

--- email_lang.php
+++ email_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['email_must_be_array']         = "Metoda walidacji e-maila musi być przekazana w tablicy.";
+$lang['email_must_be_array']         = "Metoda weryfikacji e-maila musi być przekazana w tablicy.";
 $lang['email_invalid_address']       = "Niepoprawny adres e-mail: %s";
 $lang['email_attachment_missing']    = "Nie można zlokalizować następujących załączników: %s";
 $lang['email_attachment_unreadable'] = "Nie można otworzyć następujących załączników: %s";
@@ -10,13 +10,13 @@ $lang['email_send_failure_sendmail'] = "Nie można wysłać e-maila za pomocą P
 $lang['email_send_failure_smtp']     = "Nie można wysłać e-maila za pomoc PHP SMTP. Twój serwer może nie być skonfigurowany by wysyłać e-maile za pomocą tej metody.";
 $lang['email_sent']                  = "Twój e-mail został pomyślnie wysłany za pomocą metody: %s";
 $lang['email_no_socket']             = "Nie można otworzyć socket'a do Sendmail. Proszę sprawdzić ustawienia.";
-$lang['email_no_hostname']           = "Nie podano nazwy hostu SMTP.";
+$lang['email_no_hostname']           = "Nie podano nazwy hosta SMTP.";
 $lang['email_smtp_error']            = "Wystąpił następujący błąd SMTP: %s";
 $lang['email_no_smtp_unpw']          = "Błąd: Należy podać nazwę użytkownika i hasło SMTP.";
 $lang['email_failed_smtp_login']     = "Błąd przy wysyłaniu komendy AUTH LOGIN. Błąd: %s";
 $lang['email_smtp_auth_un']          = "Błąd autentykacji nazwy użytkownika. Błąd: %s";
 $lang['email_smtp_auth_pw']          = "Błąd autentykacji hasła. Błąd: %s";
-$lang['email_smtp_data_failure']     = "Nie można wysłąć danych: %s";
+$lang['email_smtp_data_failure']     = "Nie można wysłać danych: %s";
 $lang['email_exit_status']           = "Status kodu wyjścia: %s";
 
 
